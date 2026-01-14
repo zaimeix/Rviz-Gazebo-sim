63edae8a5fb69affe31758aff1a5c8ebe6b49b5f
Zaimei Xing <zx110@students.waikato.ac.nz>
2026-01-14 14:05:00 +1300

    change ROS2 version to Hamble version

--- Rviz-Gazebo-sim/src/waikato_robot_sim_moveit/launch/gazebo_moveit.launch.py
+++ Rviz-Gazebo-sim/src/waikato_robot_sim_moveit/launch/gazebo_moveit.launch.py
@@ -9,8 +9,6 @@ from launch.substitutions import Command
 from launch.actions import LogInfo
 from launch.actions import ExecuteProcess
 from launch.actions import SetEnvironmentVariable
-from launch.substitutions import LaunchConfiguration
-from launch.actions import LogInfo
 from ament_index_python.packages import get_package_share_directory
 from launch.substitutions import FindExecutable
 from launch import LaunchContext
@@ -24,7 +22,7 @@ import common
 '''
 since gazebo controller conflicts with controlle_manager,
 the original controller_manager should be removed when starting gazebo controller.
-and, due to gazebo uses different hardware interface, a set of seperate controller
+and, due to gazebo uses different hardware interface, a set of seperate controller 
 interface description is used.
 '''
 
@@ -41,7 +39,7 @@ def generate_launch_description():
     robot_cfg_control_macro_name = common.ROBOT_CONFIG_CONTROL_MACRO_NAME
 
     pkg_name = common.PKG_NAME
-    print("descriptor: ", robot_description_file_name);
+
     # Declare arguments
     declared_arguments = []
     declared_arguments.append(
@@ -51,46 +49,47 @@ def generate_launch_description():
             description="Use simulation clock",
         )
     )
-
+    
     use_sim_time = LaunchConfiguration("use_sim_time")
     pkg_share = FindPackageShare(pkg_name)
-
+    
     initial_positions_file = PathJoinSubstitution([
         pkg_share,
         robot_cfg_path,
         "initial_positions.yaml"
     ])
-
+    
     robot_description_file = PathJoinSubstitution([
-        FindPackageShare(robot_description_pkg_name),
+        FindPackageShare(robot_description_pkg_name), 
         "urdf",
         robot_description_file_name
     ])
-
+    
     xacro_file = PathJoinSubstitution([
         pkg_share,
-        robot_cfg_path,
+        robot_cfg_path, 
         robot_cfg_main_xacro_name
     ])
 
     control_xacro_file = PathJoinSubstitution([
         pkg_share,
-        robot_cfg_path,
+        robot_cfg_path, 
         robot_cfg_ros2_ctl_xacro_name
     ])
 
     control_yaml_file = PathJoinSubstitution([
         pkg_share,
-        robot_cfg_path,
+        robot_cfg_path, 
         robot_cfg_controller_yaml_name
     ])
 
     launch_moveit_file = PathJoinSubstitution([
         pkg_share,
-        "launch",
+        "launch", 
         "move_group.launch.py"
     ])
 
+    
     robot_description_content = ParameterValue(Command([
         'xacro ',
         xacro_file,
@@ -101,7 +100,7 @@ def generate_launch_description():
         ' control_macro_name:=', robot_cfg_control_macro_name,
         ' initial_positions_file:=', initial_positions_file,
     ]),value_type=str)
-
+    
     robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -113,27 +112,39 @@ def generate_launch_description():
             'publish_frequency': 50.0
         }]
     )
-
+    
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             PathJoinSubstitution([
-                FindPackageShare("ros_gz_sim"),
-                "launch",
-                "gz_sim.launch.py"
+                FindPackageShare("gazebo_ros"), 
+                "launch", 
+                "gazebo.launch.py"  # Combined server + client
             ])
         ]),
         launch_arguments={
-            "gz_args": "-r empty.sdf",
+            "world": PathJoinSubstitution([
+                FindPackageShare("gazebo_ros"), 
+                "worlds", 
+                "empty.world"
+            ]),
+            "verbose": "true",
             "use_sim_time": use_sim_time,
         }.items(),
+    )
+
+    static_tf = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        arguments=["0", "0", "0", "0", "0", "0", "world", "base_link"],
+        parameters=[{"use_sim_time": use_sim_time}],
     )
 
     gazebo_spawn_entity = TimerAction(
         period=4.0,
         actions=[
             Node(
-                package="ros_gz_sim",
-                executable="create",
+                package="gazebo_ros",
+                executable="spawn_entity.py",
                 arguments=[
                     "-entity", robot_name,
                     "-topic", "robot_description",
@@ -142,13 +153,6 @@ def generate_launch_description():
                 output="screen",
             )
         ]
-    )
-
-    static_tf = Node(
-        package="tf2_ros",
-        executable="static_transform_publisher",
-        arguments=["0", "0", "0", "0", "0", "0", "world", "base_link"],
-        parameters=[{"use_sim_time": use_sim_time}],
     )
 
     spawn_joint_state_broadcaster = TimerAction(
@@ -188,9 +192,9 @@ def generate_launch_description():
             )
         ]
     )
-
+    
     rviz_node = TimerAction(
-        period=16.0,
+        period=12.0,
         actions=[
             Node(
                 package="rviz2",
@@ -211,8 +215,8 @@ def generate_launch_description():
         gazebo,
         static_tf,
         gazebo_spawn_entity,
-        #spawn_joint_state_broadcaster,
-        #spawn_manipulator_controller,
-        #move_group,
-        #rviz_node,
+        spawn_joint_state_broadcaster,
+        spawn_manipulator_controller,
+        move_group,
+        rviz_node,
     ])
